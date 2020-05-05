6aaf80d2d685cee05dc297ce05f1b22c24a83ab5
dtamayo <tamayo.daniel@gmail.com>
2020-05-05 13:39:24 -0400

    modified plots and added Zhou and Quillen features

--- spock/additional_feature_functions.py
+++ spock/additional_feature_functions.py
@@ -88,10 +88,42 @@ def additional_features(sim, args): # final cut down list
             features['beta'+label] = (ps[i2].a-ps[i1].a)/RH
             features["AMDcrit"+label] = AMD_crit(sim, i1, i2)
             features["EMcross"+label] = (ps[i2].a-ps[i1].a)/ps[i1].a       
-            features["j"+label], features["k"+label], _ = find_strongest_MMR(sim, i1, i2)
-        
+            features["j"+label], features["k"+label], _ = find_strongest_MMR(sim, i1, i2) 
         triofeatures.append(features)
     
+    features["e1Z07"] = ps[1].e * (ps[2].a+ps[1].a) / (ps[2].a-ps[1].a)
+    features["e2Z07"] = ps[2].e * (ps[3].a+ps[2].a) / (ps[3].a-ps[2].a)
+    features["e3Z07"] = ps[3].e * (ps[3].a+ps[2].a) / (ps[3].a-ps[2].a)
+    features["eavgZ07inner"] = np.mean([features['e1Z07'], features['e2Z07']])
+    features["eavgZ07outer"] = np.mean([features['e2Z07'], features['e3Z07']])
+    features["eavgZ07"] = np.mean([features["e1Z07"], features["e2Z07"], features["e3Z07"]])
+    features["muavgZ07inner"] = (ps[1].m+ps[2].m)/ps[0].m/2 # mean of the mass ratios
+    features["muavgZ07outer"] = (ps[2].m+ps[3].m)/ps[0].m/2 # mean of the mass ratios
+    features["muavgZ07"] = np.mean([ps[1].m, ps[2].m, ps[3].m])
+    features["kZ07inner"] = (ps[2].a-ps[1].a) * 2. / (ps[2].a + ps[1].a) / (2.*features["muavgZ07inner"]/3.)**(1./3.)
+    features["kZ07outer"] = (ps[3].a-ps[2].a) * 2. / (ps[3].a + ps[2].a) / (2.*features["muavgZ07outer"]/3.)**(1./3.)
+    features["kZ07avg"] = np.mean([features['kZ07inner'], features['kZ07outer']])
+    features["AZ07inner"] = -2. + features["eavgZ07inner"] - 0.27*np.log10(features["muavgZ07inner"]) # Zhou 2007 Eq 4
+    features["AZ07outer"] = -2. + features["eavgZ07outer"] - 0.27*np.log10(features["muavgZ07outer"]) # Zhou 2007 Eq 4
+    features["AZ07avg"] = -2. + features["eavgZ07"] - 0.27*np.log10(features["muavgZ07"]) # Zhou 2007 Eq 4
+    features["BZ07inner"] = 18.7 + 1.1*np.log10(features["muavgZ07inner"]) - (16.8 + 1.2*np.log10(features["muavgZ07inner"]))*features['eavgZ07inner'] # Zhou 2007 Eq 4
+    features["BZ07outer"] = 18.7 + 1.1*np.log10(features["muavgZ07outer"]) - (16.8 + 1.2*np.log10(features["muavgZ07outer"]))*features['eavgZ07outer'] # Zhou 2007 Eq 4
+    features["BZ07avg"] = 18.7 + 1.1*np.log10(features["muavgZ07"]) - (16.8 + 1.2*np.log10(features["muavgZ07"]))*features['eavgZ07'] # Zhou 2007 Eq 4
+    features["Z07log_instability_time_inner"] = features["AZ07inner"] + features['BZ07inner']*np.log10(features['kZ07inner']/2.3)
+    features["Z07log_instability_time_outer"] = features["AZ07outer"] + features['BZ07outer']*np.log10(features['kZ07outer']/2.3)
+    features["Z07log_instability_time_avg"] = features["AZ07avg"] + features['BZ07avg']*np.log10(features['kZ07avg']/2.3)
+    features["Z07Stable_avg"] = features["Z07log_instability_time_avg"] > 9
+    features["Z07Stable_worstpair"] = min(features["Z07log_instability_time_inner"], features["Z07log_instability_time_outer"]) > 9
+
+    features["deltaQ11inner"] = (ps[2].a-ps[1].a)/ps[2].a
+    features["deltaQ11outer"] = (ps[3].a-ps[2].a)/ps[3].a
+    features["deltaQ11avg"] = np.mean([features['deltaQ11inner'], features['deltaQ11outer']])
+    features["Q11log_instability_time_inner"] = np.log10(features["deltaQ11inner"]**8 / np.abs(np.log(features["deltaQ11inner"]))**3 / features["muavgZ07inner"]**3 / 8.) # Qullen 2011 Eq 68
+    features["Q11log_instability_time_outer"] = np.log10(features["deltaQ11outer"]**8 / np.abs(np.log(features["deltaQ11outer"]))**3 / features["muavgZ07outer"]**3 / 8.) # Qullen 2011 Eq 68
+    features["Q11log_instability_time_avg"] = np.log10(features["deltaQ11avg"]**8 / np.abs(np.log(features["deltaQ11avg"]))**3 / features["muavgZ07"]**3 / 8.) # Qullen 2011 Eq 68
+    features["Q11Stable_avg"] = features['Q11log_instability_time_avg'] > 9
+    features["Q11Stable_worstpair"] = min(features['Q11log_instability_time_inner'], features["Q11log_instability_time_outer"]) > 9
+
     triopairs, triojks, trioa10s, triotseries = init_sim(sim, trios)
     stable = additional_get_tseries(sim, Norbits, Nout, trios, triopairs, triojks, trioa10s, triotseries)
 
