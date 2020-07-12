0b20506851b6e1e3834e9532e89e0ba670602eae
Daniel Tamayo <tamayo.daniel@gmail.com>
2020-07-12 11:56:26 -0400

    removed githash from version.py

--- spock/__init__.py
+++ spock/__init__.py
@@ -4,9 +4,5 @@
 from .featureclassifier import FeatureClassifier
 from .nbodyregressor import NbodyRegressor
 from .version import __version__
-try:
-    from .version import __githash__
-except:
-    pass
 
 __all__ = ["FeatureClassifier", "NbodyRegressor"]
--- spock/version.py
+++ spock/version.py
@@ -1,6 +1,1 @@
 __version__ = '1.0.5'
-try:
-    import subprocess
-    __githash__ = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
-except:
-    pass
