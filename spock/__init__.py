14bc5fe11644ee069ca2b59c30b382b59b73e491
Daniel Tamayo <tamayo.daniel@gmail.com>
2020-07-12 11:49:53 -0400

    put githash import in init in try except for pip install

--- spock/__init__.py
+++ spock/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """Stability of Planetary Orbital Configurations Klassifier"""
 
-from .version import __version__, __githash__
 from .featureclassifier import FeatureClassifier
 from .nbodyregressor import NbodyRegressor
+from .version import __version__
+try:
+    from .version import __githash__
+except:
+    pass
 
 __all__ = ["FeatureClassifier", "NbodyRegressor"]
