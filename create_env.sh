1f086aac635198a3117ea5ebad8556f7e66510b9
MilesCranmer <miles.cranmer@gmail.com>
2021-01-05 13:22:36 -0500

    Remove version requirements for some packages

--- create_env.sh
+++ create_env.sh
@@ -1,3 +1,3 @@
-conda create -y --name spock -c pytorch -c conda-forge python=3.8 numpy scipy pandas scikit-learn matplotlib pytorch torchvision torchaudio cudatoolkit=10.1 numba dask xgboost tqdm dill jupyter
-conda activate spock
-pip install celluloid icecream einops fire celmech rebound==3.9.0 pytorch_lightning==0.9.0 seaborn==0.11.0
+conda create -y --name spock -c pytorch -c conda-forge python=3.8 numpy scipy pandas scikit-learn matplotlib pytorch torchvision torchaudio cudatoolkit=10.1 numba dask xgboost tqdm dill jupyter rebound seaborn fire einops &&
+    conda activate spock &&
+    pip install celluloid icecream celmech pytorch_lightning==0.9.0 
