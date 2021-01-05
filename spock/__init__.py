1d648d959f4943f5e7d85314e1ffeec5ef78666b
MilesCranmer <miles.cranmer@gmail.com>
2021-01-05 13:23:07 -0500

    Add regressor to init file

--- spock/__init__.py
+++ spock/__init__.py
@@ -2,7 +2,8 @@
 """Stability of Planetary Orbital Configurations Klassifier"""
 
 from .featureclassifier import FeatureClassifier
+from .regression import FeatureRegressor
 from .nbodyregressor import NbodyRegressor
 from .version import __version__
 
-__all__ = ["FeatureClassifier", "NbodyRegressor"]
+__all__ = ["FeatureClassifier", "FeatureRegressor", "NbodyRegressor"]
