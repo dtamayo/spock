a5db0361164d6402b2ae71cd55ffbc2c72b3a6ab
MilesCranmer <miles.cranmer@gmail.com>
2021-01-06 00:15:04 -0500

    Missing pandas requirement

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ classifier_requirements = [
 regression_requirements = [
     'matplotlib', 'pytorch_lightning>=1.0.0', 'torch>=1.5.1', 'torchvision>=0.6.1',
     'scipy', 'rebound', 'scikit-learn', 'einops', 'matplotlib', 'numpy',
-    'celmech'
+    'celmech', 'pandas'
 ]
 
 exec(open('spock/version.py').read())
