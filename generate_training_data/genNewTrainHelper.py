58881383c58b9897e9cfddb10cbe2cf9341e0b75
Elio Thadhani <eliothadhani@outlook.com>
2024-07-17 03:32:03 +0000

    removed redundant file and fixed pool

--- generate_training_data/genNewTrainHelper.py
+++
@@ -1,8 +0,0 @@
-from spock import featureclassifier
-
-#this file is to get around the jupyter notebook bug with multiprocessing
-
-spock = featureclassifier.FeatureClassifier()
-
-def getSpock(sim):
-    return spock.generate_features(sim)
